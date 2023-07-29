0c0c34e17269b6137925547cd01d48fb51567579
matin <elliotmigh@gmail.com>
2023-07-29 11:59:51 +0330

    create function for click battery card item and change shape type and change icon tint

--- app/src/main/java/com/example/tesla/ActivityInfoCyberTruck.kt
+++ app/src/main/java/com/example/tesla/ActivityInfoCyberTruck.kt
@@ -1,8 +1,12 @@
 package com.example.tesla
 
+import android.content.Intent
+import android.graphics.drawable.shapes.Shape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.tesla.databinding.ActivityInfoCyberTruckBinding
+import soup.neumorphism.ShapeType
 
 class ActivityInfoCyberTruck : AppCompatActivity() {
     //create binding:
@@ -11,5 +15,24 @@ class ActivityInfoCyberTruck : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityInfoCyberTruckBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //go to activity battery:
+        goToBatteryActivity()
+    }
+
+    private fun goToBatteryActivity() {
+        binding.neumorphBattery.setOnClickListener {
+            //edit shape type when clicked:
+            binding.neumorphBattery.setShapeType(ShapeType.PRESSED)
+
+//            edit icon tint color when clicked:
+                binding.neumorphBattery.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+            //create intent for going another activity:
+            val intentBattery = Intent(this, ActivityBattery::class.java)
+            startActivity(intentBattery)
+        }
     }
 }
